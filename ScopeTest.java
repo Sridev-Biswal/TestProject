454082b0bbefcd996f8992ed9ba6521a5eecdaf7
Sridev-Biswal <53298700+Sridev-Biswal@users.noreply.github.com>
2019-07-28 20:25:37 +0530

    SCOPE

---
+++ ScopeTest.java
@@ -0,0 +1,27 @@
+package com.maren.demosecb.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ScopeTest
+ */
+@WebServlet("/scopeTest")
+public class ScopeTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("REQUEST", "Request Scope");
+		request.getSession().setAttribute("SESSION", "Session Scope");
+		getServletContext().setAttribute("APP", "Application Scope");
+		request.getRequestDispatcher("/ScopeTest2").forward(request, response);
+	}
+
+}
